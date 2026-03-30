3d60efec86540e76fa611dcdddf6cd16ff2cd965
Rafael Faustini <contato@rafaelfaustini.com.br>
2026-03-30 01:36:31 +0100

    This used to be a bug and should trigger a test to fail

--- src/components/FileExplorer/FileExplorer.tsx
+++ src/components/FileExplorer/FileExplorer.tsx
@@ -15,7 +15,7 @@ function FileExplorer() {
 
     async function handleFileSystemItemDoubleClick(fileSystemObject: FileSystemObject) {
         if(fileSystemObject.isFolder) {
-            const separator = currentPath.includes('\\') ? '\\' : '/';
+            const separator = '/';
             let newPath = currentPath + separator + fileSystemObject.name;
             handlePathChange(newPath);
         }
